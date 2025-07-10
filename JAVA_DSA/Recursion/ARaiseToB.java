9753fb184fc1766ff32554714144f5510e778194
ARCHITRAGHAV <architraghav2004@gmail.com>
2025-07-10 18:05:53 +0530

    logarithmic time complexity method done

--- JAVA_DSA/Recursion/ARaiseToB.java
+++ JAVA_DSA/Recursion/ARaiseToB.java
@@ -9,10 +9,17 @@ public class ARaiseToB {
         return a * pow(a, b - 1);
     }
 
+    public static int pow1(int a, int b) {
+        if (b == 1) return a;
+        if (b % 2 == 0) return pow1(a, b / 2) * pow1(a, b / 2);
+        else return pow1(a, b / 2) * pow1(a, b / 2) * a;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         System.out.println(pow(a, b));
+        System.out.println(pow1(a, b));
     }
 }
